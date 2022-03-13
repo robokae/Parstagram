1d82f3d95f09330a94d35d04b34412037b4ca649
robokae <alexanderhom19@gmail.com>
2022-03-13 01:03:09 -0800

    implement login using Parse

---
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("JNxzlKcA2IYNwKbylQD645N4H7nsLa9bCXI68wpe")
+                .clientKey("E9pmoWbbk5zDVoVKbX6prKB8Lj9OuS9kK089jepZ")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+
+}
